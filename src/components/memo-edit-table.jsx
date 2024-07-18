5828b12dd0738aee2902ea41d2e8fc435363bc55
jun-kondo <ranmaru.oo.q@gmail.com>
2024-07-18 16:25:42 +0900

    [Delete]編集コンポーネントのformタグ

--- src/components/memo-edit-table.jsx
+++ src/components/memo-edit-table.jsx
@@ -4,24 +4,22 @@ export default function MemoEditTable({ selectedMemo, onEdit, onDelete }) {
   const [text, setText] = useState(selectedMemo.content);
   return (
     <section className="edit-table">
-      <form>
-        <textarea value={text} onChange={(e) => setText(e.target.value)} />
-        <br />
-        <button
-          onClick={() => {
-            onEdit(selectedMemo.id, text);
-          }}
-        >
-          編集
-        </button>
-        <button
-          onClick={() => {
-            onDelete(selectedMemo.id);
-          }}
-        >
-          削除
-        </button>
-      </form>
+      <textarea value={text} onChange={(e) => setText(e.target.value)} />
+      <br />
+      <button
+        onClick={() => {
+          onEdit(selectedMemo.id, text);
+        }}
+      >
+        編集
+      </button>
+      <button
+        onClick={() => {
+          onDelete(selectedMemo.id);
+        }}
+      >
+        削除
+      </button>
     </section>
   );
 }
