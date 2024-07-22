a664b54653754dc28c5d942f892d40de98ea0dc9
jun-kondo <ranmaru.oo.q@gmail.com>
2024-07-22 20:19:59 +0900

    [Modify]jsxファイル名
    バスカルケースで統一

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import MemoApp from "./components/MemoApp";
+import LoginContent from "./components/LoginContent";
+import LoginButton from "./LoginButton";
 
 export default function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  const loginConfig = {
+    isLogin,
+    toggleLogin: () => {
+      setIsLogin(!isLogin);
+    },
+  };
+
   return (
     <>
-      <MemoApp />
+      <LoginContent.Provider value={loginConfig}>
+        <LoginButton />
+        <MemoApp />
+      </LoginContent.Provider>
     </>
   );
 }
