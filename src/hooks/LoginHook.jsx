26ff350e8572acaabe72f6c458c2f5cae80579e0
jun-kondo <ranmaru.oo.q@gmail.com>
2024-07-23 15:48:46 +0900

    [Delete]不要なコメント

--- src/hooks/LoginHook.jsx
+++ src/hooks/LoginHook.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { createContext, useContext } from "react";
 
 const LoginContext = createContext({});
+
 export const LoginProvider = ({ children }) => {
   const [isLogin, setIsLogin] = useState(false);
   const loginConfig = {
@@ -18,6 +19,4 @@ export const LoginProvider = ({ children }) => {
   );
 };
 
-// valueの値(この場合はloginConfig)が渡される
-// useLoginStatusはログインに関するstate変数や更新関数を持ったcustom hook
 export const useLoginStatus = () => useContext(LoginContext);
