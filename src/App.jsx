affaa7201dfafc7dd0d6798b29faf25e7bf261a6
jun-kondo <ranmaru.oo.q@gmail.com>
2024-07-26 19:23:29 +0900

    [Modify]微修正
    カスタムフックのパスが間違っていたため修正
    ログイン中かどうかのhタグを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import MemoApp from "./components/MemoApp";
-import { LoginProvider } from "providers/LoginProvider";
+import { LoginProvider } from "./providers/LoginProvider";
 import LoginButton from "./LoginButton";
 
 export default function App() {
--- src/components/MemoApp.jsx
+++ src/components/MemoApp.jsx
@@ -1,6 +1,7 @@
 import MemoList from "./MemoList";
 import MemoForm from "./MemoForm";
 import useMemos from "../hooks/useMemos";
+import useLoginConfig from "../hooks/useLoginConfig";
 
 export default function MemoApp() {
   const {
@@ -11,9 +12,11 @@ export default function MemoApp() {
     handleEditMemo,
     handleDeleteMemo,
   } = useMemos();
+  const { isLogin } = useLoginConfig();
   return (
     <>
-      <h1>{selectedMemo ? "編集" : "一覧"}</h1>
+      <h1>{isLogin ? "ログイン中" : "未ログイン"}</h1>
+      <h2>{isLogin && (selectedMemo ? "編集" : "一覧")}</h2>
       <MemoList
         memos={memoList}
         onSelect={(memoId) => {
